860e0bc7830006e14190e7566997edd5c5e727a2
mmadishetty <mmadishetty@ivycomptech.com>
2020-12-11 17:33:22 +0530

    commit info: start and end nav calculations

--- AppService.js
+++ AppService.js
@@ -18,14 +18,14 @@ class AppService
       var TR = (endVal - startVal)/startVal;
       var lhs = 1+TR;
       var rhs = 1/years;
-      return Math.round(100*(Math.pow(lhs, rhs)-1));
+      return Math.round(10000*(Math.pow(lhs, rhs)-1))/100;
     }
     getCalculatedReturnsStr(MFData,  period , mfId ,dateStart ,dateEnd )
     {
         var currDate = new Date(dateEnd);
 
         var str = "";//"<table>";
-        str+= "<thead><tr><th>Month</th><th>Returns</th><th>calculation</th></tr></thead>"
+        str+= "<thead><tr><th>Month</th><th>Returns</th><th>start Nav</th><th>End Nav</th><th>Start Date</th><th>End Date</th></tr></thead>"
         
         
         str+= "<tbody>";
@@ -62,14 +62,46 @@ class AppService
 
          //Returns
          strRow+= "<td>";
-         strRow+= endNavDate.toString("MMM-yy");
+         strRow+= this.getReturnsForPeriod(startNavDate, endNavDate,1);
          strRow+= "</td>";
 
          //Month
          strRow+= "<td>";
-         strRow+= ("Start Nav: "+startNavDate.toString("dd-MMM-yy")+"\t\t");
-         strRow+= ("End Nav: "+endNavDate.toString("dd-MMM-yy"));strRow+= "</td>";
+         strRow+= (startNavDate.toString("dd-MMM-yy")+"\t\t");
+         strRow+= "</td>";
+        
+         strRow+= "<td>";
+         strRow+= (endNavDate.toString("dd-MMM-yy"));
+         strRow+= "</td>";
         return strRow;
+    }
+    getReturnsForPeriod = function(startNavDate, endNavDate,years)
+    {
+        var MFEndDate = new Date(MFData["endDate"]);
+        var MFStartDate = new Date(MFData["startDate"]);
+        if(formatter.compareDates(startNavDate,MFStartDate)==1)
+            startNavDate = MFStartDate;
+        if(formatter.compareDates(endNavDate,MFEndDate)==-1)
+            endNavDate = MFEndDate;
+        var startNav = this.getNAVForDate(startNavDate);
+        var endNav = this.getNAVForDate(endNavDate);
+        startNav = Math.round(startNav * 100) / 100;
+        endNav = Math.round(endNav*100)/100;
+        var str = this.getReturns(startNav ,endNav ,1);
+        str+= ("%" + "</td><td>"+startNav + "</td><td>"+endNav);
+        return str;        
+    }
+    getNAVForDate = function(date)
+    {
+        if(MFData[date.toString("dd-MM-yyyy")])
+            return MFData[date.toString("dd-MM-yyyy")];
+        for(var i=0;i<10;i++)
+        {
+            date.addDays(-1);
+            if(MFData[date.toString("dd-MM-yyyy")])
+                return MFData[date.toString("dd-MM-yyyy")];
+        }
+        return 1;
     }
   
 }
